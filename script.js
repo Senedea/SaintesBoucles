77681b65a72e2ca1b1db96fa348b52c83019106a
Alex <alexis.cappoen@gmail.com>
2021-06-04 21:56:19 +0200

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+var deroulant=document.getElementById("deroulant")
+
+deroulant.addEventListener("click",function(){
+ var sousmenu=document.querySelector("#sousmenu")
+ if(sousmenu.classList.contains("toggle")){
+    deroulant.innerHTML="Boucles ▲"
+}
+else{
+    deroulant.innerHTML="Boucles ▼"
+}
+     sousmenu.classList.toggle("toggle")
+
+})
